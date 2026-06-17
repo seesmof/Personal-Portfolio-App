a1e52b1dbdf2897948492dbad636437666a4f2d9
Oleg Onyshchenko <seesmwork@gmail.com>
2026-06-17 18:53:09 +0300

    feat: add the certificates, make the additional education link to them

--- src/components/index/AdditionalEducationSection.tsx
+++ src/components/index/AdditionalEducationSection.tsx
@@ -1,16 +1,21 @@
+import Link from "next/link";
+
 interface Education {
   title: string;
   description: string;
+  certificateHref: string;
 }
 
 const educations: Education[] = [
   {
     title: "Networking Basics",
     description: "A networking course by Cisco. 2025",
+    certificateHref: "cisco",
   },
   {
     title: "HTML & CSS Course",
     description: "A frontend templating course by Freshcode. 2023",
+    certificateHref: "freshcode",
   },
 ];
 
@@ -19,10 +24,16 @@ export default function AdditionalEducationSection() {
     <section className="bg-white roundmd p-3 flex flex-col gap-3">
       <h2 className="font-semibold text-lg sm:text-xl">Additional Education</h2>
       {educations.map((education, index) => (
-        <div className="flex flex-col gap-1" key={index}>
-          <h3 className="font-bold">{education.title}</h3>
+        <Link
+          href={`/certificates/${education.certificateHref}.pdf`}
+          className="flex flex-col gap-1 group"
+          key={index}
+        >
+          <h3 className="font-bold group-hover:underline underline-offset-4">
+            {education.title}
+          </h3>
           <p className="text-stone-600 text-sm">{education.description}</p>
-        </div>
+        </Link>
       ))}
     </section>
   );
