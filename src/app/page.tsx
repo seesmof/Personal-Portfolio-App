db473b2817871a56fb618f0a1cb1a148e9f2c81e
Oleg Onyshchenko <seesmwork@gmail.com>
2026-06-17 19:13:55 +0300

    refactor: extract hero section to its own component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,16 @@
-import AdditionalEducationSection from "@/components/index/AdditionalEducationSection";
-import EducationSection from "@/components/index/EducationSection";
-import ProjectsSection from "@/components/index/ProjectsSection";
-import SkillsSection from "@/components/index/SkillsSection";
 import Navbar from "@/components/Navbar";
+import HeroSection from "@/components/index/HeroSection";
+import ProjectsSection from "@/components/index/ProjectsSection";
+import EducationSection from "@/components/index/EducationSection";
+import AdditionalEducationSection from "@/components/index/AdditionalEducationSection";
+import SkillsSection from "@/components/index/SkillsSection";
 
 export default function HomePage() {
   return (
     <div className="min-h-screen bg-green-50">
       <div className="flex mx-auto max-w-3xl flex-col gap-3 p-3">
         <Navbar />
-
-        {/* Hero Section */}
-        <section className="flex flex-col bg-white rounded-md p-3 py-24 justify-center items-center">
-          <h1 className="font-bold text-3xl sm:text-4xl">
-            Christian Frontend Developer
-          </h1>
-          <p className="mt-3 text-stone-600">
-            Working for the glory of our Lord Jesus Christ.
-          </p>
-        </section>
-
+        <HeroSection />
         <div className="flex sm:flex-row flex-col gap-3">
           <ProjectsSection />
           <div className="flex flex-col gap-3">
---
+++ src/components/index/HeroSection.tsx
@@ -0,0 +1,12 @@
+export default function HeroSection() {
+  return (
+    <section className="flex flex-col bg-white rounded-md p-3 py-24 justify-center items-center">
+      <h1 className="font-bold text-3xl sm:text-4xl">
+        Christian Frontend Developer
+      </h1>
+      <p className="mt-3 text-stone-600">
+        Working for the glory of our Lord Jesus Christ.
+      </p>
+    </section>
+  );
+}
